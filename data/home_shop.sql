3b588263c11a80055217e3327cfbb5f9f7828d3d
Jaiver Ariza <jeag-1995@hotmail.com>
2020-05-23 03:28:42 -0500

    Database

---
+++ data/home_shop.sql
@@ -0,0 +1,86 @@
+/*
+SQLyog Ultimate v11.11 (32 bit)
+MySQL - 5.5.5-10.1.37-MariaDB : Database - home_shop
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`home_shop` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `home_shop`;
+
+/*Table structure for table `companies` */
+
+DROP TABLE IF EXISTS `companies`;
+
+CREATE TABLE `companies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  `representative` varchar(30) NOT NULL,
+  `identification` varchar(10) NOT NULL,
+  `area` varchar(20) DEFAULT NULL,
+  `description` text,
+  `keywords` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `orders` */
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `company` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `company` (`company`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`company`) REFERENCES `companies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `orders_details` */
+
+DROP TABLE IF EXISTS `orders_details`;
+
+CREATE TABLE `orders_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idProduct` int(11) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `idOrder` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idProduct` (`idProduct`),
+  KEY `order_products_ibfk_1` (`idOrder`),
+  CONSTRAINT `orders_details_ibfk_1` FOREIGN KEY (`idOrder`) REFERENCES `orders` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `products` */
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `unity` varchar(20) NOT NULL,
+  `description` text,
+  `price` double NOT NULL,
+  `company` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `companies_fk` (`company`),
+  CONSTRAINT `companies_fk` FOREIGN KEY (`company`) REFERENCES `companies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

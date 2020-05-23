bb9792c1bb3367d76464061cd327880608116f1f
Jaiver Ariza <jeag-1995@hotmail.com>
2020-05-22 19:25:50 -0500

    Company controller

--- application/controllers/Company.php
+++ application/controllers/Company.php
@@ -5,7 +5,7 @@ require_once(APPPATH.'/libraries/REST_Controller.php' );
 use Restserver\libraries\REST_Controller;
 
 
-class Products extends REST_Controller {
+class Company extends REST_Controller {
 
 	public function __construct(){
 		header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
@@ -17,22 +17,15 @@ class Products extends REST_Controller {
 		$this->load->database();
 	}
 
-	public function search_get($page = 0, $filter = "", $company = 0) {
-		$page = $page * 10;
-
-		if ($company) $cond = "AND p.company = $company";
-		else $cond = "OR c.name LIKE '%$filter%'";
-		if ($filter === "0") $filter = "";
-	
-		$sql = "SELECT p.id, p.name, p.unity, p.price, p.company, p.description, c.name AS 'companyName' FROM products AS p
-				INNER JOIN companies AS c on (p.company = c.id)
-				WHERE p.name LIKE '%$filter%' $cond LIMIT $page,10;";
+	public function info_get($company = 0) {
+		$sql = "SELECT id, name, description FROM companies WHERE id = $company;";
 
 		$query = $this->db->query($sql);
 
+		$company = $query->result_array();
 		$response = array(
 			'error' => FALSE,
-			'products' => $query->result_array()
+			'company' => $company[0]
 		);
 	
 		$this->response($response);
